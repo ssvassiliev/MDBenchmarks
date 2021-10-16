d7f9d77715ce054e6d708a855adb9e254987a0a6
Serguiei Vassiliev <svassiliev1@gmail.com>
2021-10-16 20:29:07 -0300

    Add list filters

--- mdbench/admin.py
+++ mdbench/admin.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 from .models import SimulationInput, Benchmark, BenchmarkInstance, SerialBenchmarkInstance, Software, CPU, GPU, Resource, Site
 
 admin.site.register(Benchmark)
-admin.site.register(BenchmarkInstance)
 admin.site.register(SerialBenchmarkInstance)
-admin.site.register(Software)
 admin.site.register(CPU)
 admin.site.register(GPU)
 admin.site.register(Resource)
 admin.site.register(Site)
 admin.site.register(SimulationInput)
+
+@admin.register(Software)
+class SoftwareAdmin(admin.ModelAdmin):
+    list_display = ("module","module_version","toolchain")
+    list_filter = ("instruction_set",)
+
+
+@admin.register(BenchmarkInstance)
+class BenchmarkInstanceAdmin(admin.ModelAdmin):
+    list_display = ("software", "resource")
+    list_filter = ("software__module","software__instruction_set")
