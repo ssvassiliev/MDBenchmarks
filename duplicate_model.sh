e4c13c8fc55b18ec1218377e56af7e40ca9fb94b
Ubuntu <ubuntu@ubuntu.beluga-cloud>
2024-10-04 15:02:14 +0000

    python -> python3

--- duplicate_model.sh
+++ duplicate_model.sh
@@ -2,7 +2,7 @@
 # This script will create a new serial/parallel benchmark or software entry
 # by duplicating a benchmark or software with a specific user supplied PK
 # Usage: bash ./duplicate_model <serial|parallel|software> <PK>
-
+# Create directory for temporary files: mkdir -p mdbench/fixtures/
 
 if [ $# != 2 ]; then
   echo "Usage: bash ./duplicate_model.sh serial|parallel|software pk"
@@ -12,13 +12,13 @@ fi
 source ~/.bashrc
 
 if  [ $1 = "serial" ]; then
-  python manage.py dumpdata mdbench.SerialBenchmarkInstance \
+  python3 manage.py dumpdata mdbench.SerialBenchmarkInstance \
   --pks $2 --format yaml > mdbench/fixtures/tmp.yaml
 elif  [ $1 = "parallel" ]; then
-  python manage.py dumpdata mdbench.BenchmarkInstance \
+  python3 manage.py dumpdata mdbench.BenchmarkInstance \
   --pks $2 --format yaml > mdbench/fixtures/tmp.yaml
 elif  [ $1 = "software" ]; then
-  python manage.py dumpdata mdbench.Software \
+  python3 manage.py dumpdata mdbench.Software \
   --pks $2 --format yaml > mdbench/fixtures/tmp.yaml
 else
   echo 'Error: argument #1 should be "serial", "parallel", or "software"'
@@ -28,5 +28,5 @@ fi
 # Replace pk with null
 sed '2s/.*/  pk: null/' mdbench/fixtures/tmp.yaml > mdbench/fixtures/tmp2.yaml
 
-python manage.py loaddata mdbench/fixtures/tmp2.yaml
+python3 manage.py loaddata mdbench/fixtures/tmp2.yaml
 rm mdbench/fixtures/tmp*.yaml
